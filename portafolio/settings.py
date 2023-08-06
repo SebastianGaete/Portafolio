b7e1b83121285d5c84d85b61322085c90105ac1c
Sebastian Gaete <seba.gaetefuentes@gmail.com>
2023-08-06 13:51:31 -0400

    Static Update version_1.0.2

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'myportafolio/static'))
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'

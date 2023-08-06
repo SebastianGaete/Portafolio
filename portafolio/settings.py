252485570e33f75b7353daa01866ce6a7def1662
Sebastian Gaete <seba.gaetefuentes@gmail.com>
2023-08-06 14:52:04 -0400

    update settings3

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'myportfolio/static'))
+STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles/'))
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static/')]
+
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'

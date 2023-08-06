ae2e53342c9df95a3f97d4546a8f84af65fe7c9b
Sebastian Gaete <seba.gaetefuentes@gmail.com>
2023-08-06 16:52:08 -0400

    Ejecución comando collectstatic

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -124,12 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'myportfolio/static')]
 STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles/'))
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'myportfolio/static/')
-    ]
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
